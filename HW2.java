d3d2befb528af2d4a087e2b1fd82ba1e21ecbc53
Aurika <>
2022-11-22 18:55:05 +0300

    HW2

---
+++ HW2.java
@@ -0,0 +1,29 @@
+// Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение).
+
+import java.util.Scanner;
+
+public class HW2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        isPalindrome(s);
+    }
+
+    public static String reverseString(String s) {
+        String r = "";
+        for (int i = s.length() - 1; i >= 0; --i)
+            r += s.charAt(i);
+        return r;
+
+    }
+
+    public static Boolean isPalindrome(String s) {
+        if (s.equals(reverseString(s))) {
+            System.out.println("Полиндром");
+        } else {
+            System.out.println("Не полиндром");
+        }
+        return s.equals(reverseString(s));
+
+    }
+}
